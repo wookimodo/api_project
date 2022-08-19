a732b16bd11422ed6547437daf8102f3dd791f05
wookimodo <rudghk1437@naver.com>
2022-08-19 12:22:06 +0900

    add: setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setup.py 파일이 있으면 pip로 설치 가능하다.
+from setuptools import setup
+
+setup(
+    name="myapi", # 이 이름으로 패키지가 설치
+    version = "0.0.1",
+    description="my api lib",
+    url="https://github.com/wookimodo/api_project.git",
+    author="wookimodo",
+    author_email="rudghk1437@naver.com",
+    packages=["my_api"],
+    install_requires = [
+        "requests"
+    ]
+)
